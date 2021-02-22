083baf16a0db7bc5532fea3cc91d5cefb64cccaa
Luis Aponte <ldaponte98@gmail.com>
2021-02-21 23:15:30 -0500

    Definicion de la plantilla y maqueteado inicial

---
+++ resources/views/layout/buscador.blade.php
@@ -0,0 +1,25 @@
+<div class="menu-icon dw dw-menu"></div>
+<div class="search-toggle-icon dw dw-search2" data-toggle="header_search"></div>
+<div class="header-search">
+	<div class="form-group mb-0">
+		<i class="dw dw-search2 search-icon"></i>
+		<input type="text" class="form-control search-input" onkeyup="Buscar(this.value)" placeholder="Consulta aquí">
+		<div class="dropdown">
+			<a class="dropdown-toggle no-arrow" href="#" role="button" data-toggle="dropdown">
+				<i class="ion-arrow-down-c"></i>
+			</a>
+			<div class="dropdown-menu dropdown-menu-right" id="content_search">
+				<a class="link_search" href="">Bovino - 100659384728</a><hr>
+				<a class="link_search" href="">Bovino - 100659384728</a><hr>
+				<a class="link_search" href="">Bovino - 100659384728</a><hr>
+				<a class="link_search" href="">Bovino - 100659384728</a><hr>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	function Buscar(texto) {
+		 alert("Escribio "+texto)
+	}
+</script>
